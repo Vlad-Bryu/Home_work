0b98f88a379be83a3f02c1ce2e32ed468fc860d1
Vlad_Bryu <vlad@s4.team>
2019-05-29 17:12:14 +0300

    Доделал бота. Все работает

--- after_1_lesson/bot_planet.py
+++ after_1_lesson/bot_planet.py
@@ -2,6 +2,8 @@ from telegram.ext import Updater, CommandHandler
 import ephem
 import datetime
 
+
+
 PROXY = {'proxy_url': 'socks5h://t3.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
@@ -11,15 +13,15 @@ def greet_user(bot, update):
 
 
 def coordinate_of_planet(bot, update):
-    user_text = update.message.text.split()
     now = datetime.datetime.now()
-    #date = update.message.date
-    if user_text[3] == datetime.strptime(user_text[3], '%Y/%m/%d'):
+    user_text = update.message.text.split()
+    try:
+        user_text[2] == str(datetime.datetime.strptime(user_text[2], '%Y/%m/%d'))
         coord_planet = getattr(ephem, user_text[1])(user_text[2])
         coordinate = ephem.constellation(coord_planet)
         print(coordinate)
         update.message.reply_text("{} расположен/а в созвездии {} на {}".format(user_text[1], coordinate, user_text[2]))
-    else:
+    except (ValueError, IndexError):
         now = datetime.datetime.now()
         coord_planet = getattr(ephem, user_text[1])(now.strftime("%Y/%m/%d"))
         coordinate = ephem.constellation(coord_planet)
--- after_1_lesson/ephem_test.py
+++ after_1_lesson/ephem_test.py
@@ -2,9 +2,8 @@ import ephem
 import re
 import datetime
 
-
 now = datetime.datetime.now()
-coord_planet = ephem.Mars(now.strftime("%Y-%m-%d"))
+coord_planet = ephem.Mars(now.strftime("%Y/%m-%d"))
 coordinate = ephem.constellation(coord_planet)
 print(coordinate)
 print(now.strftime("%Y-%m-%d"))
