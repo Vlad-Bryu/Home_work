1ac21d8e2a2185d7c9fbe4abab4102245f2e83e9
Vlad_Bryu <vlad@s4.team>
2019-06-14 14:54:40 +0300

    Добавил модель юзера и регистрацию

---
+++ web/webapp/config.py
@@ -0,0 +1,13 @@
+from datetime import timedelta
+
+WEATHER_DEFAULT_CITY = "Moscow,Russia"
+WEATHER_API_KEY = "174c05b5083d4342a89153144193105"
+WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+#SQLALCHEMY_DATABASE_URI = "postgres://postgres:1234@localhost/new"
+
+SECRET_KEY = "sjdfh4h43uiliuaPWEIOFHeuhwP354IOR3O;:I#fasd^^#"
+
+#set FLASK_APP=webapp && set FLASK_ENV=development && set FLASK_DEBUG=1 && flask run
+
+
+REMEMBER_COOKIE_DURATION = timedelta(days=5)
